86717a6c8eb3b4594a7d099f1e81f3209241baec
Shaun <smontoya0009@gmail.com>
2025-02-02 13:52:26 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickUpLocation) {
+    return Math.abs(pickUpLocation - 42);
+}
+
+function distanceFromHqInFeet (pickUpLocation) {
+    return distanceFromHqInBlocks (pickUpLocation) * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet (start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet (start, destination) >= 400 && distanceTravelledInFeet (start, destination) < 2000) {
+        return (Math.round (100 * (0.02 * distanceTravelledInFeet (start, destination) - (400 * .02)))) / 100;
+    } else if (distanceTravelledInFeet (start, destination) >= 2000 && distanceTravelledInFeet (start, destination) < 2500) {
+        return 25;
+    } else return 'cannot travel that far';
+}
